19586cb292bb701b38f1afecfd059b02e9459ad3
Javokhir <frodonumb@gmail.com>
2019-06-26 12:29:41 +0500

    403 solved

--- src/main/java/uz/genesis/trello/config/server/ResourceServerConfiguration.java
+++ src/main/java/uz/genesis/trello/config/server/ResourceServerConfiguration.java
@@ -32,17 +32,18 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
         resources.authenticationEntryPoint(new AuthenticationFailureHandler());
     }
 
-    @Override
+    /*@Override
     public void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
-        http.requestMatchers()
-                .antMatchers(SECURED_PATTERN).and().authorizeRequests()
-                .antMatchers(AUTH_PATTERN).permitAll()
-//                .antMatchers(API_PATH + V_1 + "/users/register").permitAll()
-                .antMatchers(HttpMethod.POST, SECURED_PATTERN).access(SECURED_WRITE_SCOPE)
-                .anyRequest().access(SECURED_READ_SCOPE)
+       *//* http.requestMatchers()
+//                .antMatchers(SECURED_PATTERN).and().authorizeRequests()
+//                .antMatchers(AUTH_PATTERN).permitAll()
+//                .antMatchers(API_PATH + V_1 + "/users/*").permitAll()
+//                .antMatchers(HttpMethod.POST, SECURED_PATTERN).access(SECURED_WRITE_SCOPE)
+//                .anyRequest().access(SECURED_READ_SCOPE)
+//                .anyRequest().authenticated()
                 .and().cors()
-                .and().exceptionHandling().authenticationEntryPoint(new AuthenticationFailureHandler());
+                .and().exceptionHandling().authenticationEntryPoint(new AuthenticationFailureHandler());*//*
 
-    }
+    }*/
 }
