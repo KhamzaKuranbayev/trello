58692c17e304248d3b4b4b982ca8b73ca73d3ab8
frodonumb <frodonumb@gmail.com>
2019-07-16 18:15:07 +0300

    ownTask field added to TaskCriteria with select example

--- src/main/java/uz/genesis/trello/criterias/main/TaskCriteria.java
+++ src/main/java/uz/genesis/trello/criterias/main/TaskCriteria.java
@@ -12,16 +12,16 @@ public class TaskCriteria extends GenericCriteria {
     private Long columnId;
     private String description;
     private String deadLine;
+    private Boolean ownTask; // for example select t.* from main_project_tasks t inner join main_project_task_members m on t.id = m.task_id where m.employee_id = 11;
 
     @Builder(builderMethodName = "childBuilder")
-    public TaskCriteria(Long selfId, Integer page, Integer perPage, String sortBy, String sortDirection, String name, Long projectId, Long columnId, String description, String deadLine) {
+    public TaskCriteria(Long selfId, Integer page, Integer perPage, String sortBy, String sortDirection, String name, Long projectId, Long columnId, String description, String deadLine, Boolean ownTask) {
         super(selfId, page, perPage, sortBy, sortDirection);
         this.name = name;
         this.projectId = projectId;
         this.columnId = columnId;
         this.description = description;
         this.deadLine = deadLine;
-
-
+        this.ownTask = ownTask;
     }
 }
