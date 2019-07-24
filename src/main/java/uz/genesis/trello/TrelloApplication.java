be3f7f635db4561dfd9429f8d69a5c1e94fe3b35
frodonumb <frodonumb@gmail.com>
2019-07-24 13:49:55 +0500

    File basic implemented

--- src/main/java/uz/genesis/trello/TrelloApplication.java
+++ src/main/java/uz/genesis/trello/TrelloApplication.java
@@ -3,21 +3,26 @@ package uz.genesis.trello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import uz.genesis.trello.property.FileStorageProperties;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class TrelloApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrelloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloApplication.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TrelloApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TrelloApplication.class);
+    }
 }
---
+++ src/main/java/uz/genesis/trello/property/FileStorageProperties.java
@@ -0,0 +1,13 @@
+package uz.genesis.trello.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "file.prop")
+public class FileStorageProperties {
+
+    private String uploadDir;
+}
