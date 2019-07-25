6832492a035a12b863aa495aa81b59352f19e90b
Sherzodbek <sherzodbektatu@gmail.com>
2019-07-25 10:54:36 +0500

    Liquibase settings and xml files

--- src/main/java/uz/genesis/trello/TrelloApplication.java
+++ src/main/java/uz/genesis/trello/TrelloApplication.java
@@ -1,13 +1,18 @@
 package uz.genesis.trello;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import uz.genesis.trello.property.FileStorageProperties;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,6 +22,9 @@ import uz.genesis.trello.property.FileStorageProperties;
 })
 public class TrelloApplication extends SpringBootServletInitializer {
 
+    @Autowired
+    private DataSource dataSource;
+
     public static void main(String[] args) {
         SpringApplication.run(TrelloApplication.class, args);
     }
@@ -25,4 +33,13 @@ public class TrelloApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TrelloApplication.class);
     }
+
+    // todo Liquibase ishlashi uchun
+//    @Bean
+//    public SpringLiquibase liquibase() {
+//        SpringLiquibase liquibase = new SpringLiquibase();
+//        liquibase.setChangeLog("classpath:/db/liquibase/changelog-master.xml");
+//        liquibase.setDataSource(dataSource);
+//        return liquibase;
+//    }
 }
