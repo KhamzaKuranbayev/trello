42bd497ab2a0e06e8b1473f4c1fd58030e2e3beb
begmuhammadov_b <baxtiyorrasul@gmail.com>
2019-08-01 09:33:55 +0300

    bug fix

--- src/main/java/uz/genesis/trello/repository/settings/ErrorMessageRepository.java
+++ src/main/java/uz/genesis/trello/repository/settings/ErrorMessageRepository.java
@@ -5,6 +5,24 @@ import uz.genesis.trello.criterias.settings.ErrorMessageCriteria;
 import uz.genesis.trello.dao.GenericDao;
 import uz.genesis.trello.domain.settings.ErrorMessage;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class ErrorMessageRepository extends GenericDao<ErrorMessage, ErrorMessageCriteria> implements IErrorMessageRepository {
+
+    @Override
+    protected void defineCriteriaOnQuerying(ErrorMessageCriteria criteria, List<String> whereCause, Map<String, Object> params, StringBuilder queryBuilder) {
+        if (!utils.isEmpty(criteria.getSelfId())) {
+            whereCause.add("t.id = :selfId");
+            params.put("selfId", criteria.getSelfId());
+        }
+        if (!utils.isEmpty(criteria.getErrorCode())) {
+            whereCause.add("t.errorCode = :errorCode");
+            params.put("errorCode", criteria.getErrorCode());
+        }
+
+
+        onDefineWhereCause(criteria, whereCause, params, queryBuilder);
+    }
 }
