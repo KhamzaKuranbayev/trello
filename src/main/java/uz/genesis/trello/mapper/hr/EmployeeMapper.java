2520022a099acd02d8c83b590b8c0eaae004e9d3
norboboyev_h <humo6474@gmail.com>
2019-08-05 16:15:55 +0500

    ignore user roles from employeeDto

--- src/main/java/uz/genesis/trello/mapper/hr/EmployeeMapper.java
+++ src/main/java/uz/genesis/trello/mapper/hr/EmployeeMapper.java
@@ -1,9 +1,12 @@
 package uz.genesis.trello.mapper.hr;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.stereotype.Component;
+import uz.genesis.trello.domain.auth.User;
 import uz.genesis.trello.domain.hr.Employee;
+import uz.genesis.trello.dto.auth.UserDto;
 import uz.genesis.trello.dto.hr.EmployeeCreateDto;
 import uz.genesis.trello.dto.hr.EmployeeDto;
 import uz.genesis.trello.dto.hr.EmployeeUpdateDto;
@@ -12,5 +15,7 @@ import uz.genesis.trello.mapper.BaseMapper;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 @Component
 public interface EmployeeMapper extends BaseMapper<Employee, EmployeeDto, EmployeeCreateDto, EmployeeUpdateDto> {
-
+    @Override
+    @Mapping(ignore = true , target = "roles")
+    EmployeeDto toDto(Employee employee);
 }
